3b2e2c0dad94c401c82eabccfa1997850afad024
voldegold <kva1107@gmail.com>
2022-08-21 20:21:14 +0600

    ok

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+    public Bird() {
+        super();
+    }
+
+    @Override
+    public String getDescription() {
+        String color = "blue";
+        int numberOfPaws = 2;
+        boolean hasFur = false;
+
+       return "This animal is mostly " + color + ". " +
+               "It has " + numberOfPaws +  (numberOfPaws!=1 ? " paws" : " paw") +
+               " and " + (hasFur  ? "a " : "no ") + "fur." + " Moreover, it has 2 wings and can fly.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,14 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal {
 
+
+    public Dog() {
+       super();
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription();
+    }
 }
